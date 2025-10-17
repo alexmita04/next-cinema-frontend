4943a255bcc4599d77506585c3e78cbf9d9de6d3
alexmita04 <72689514+alexmita04@users.noreply.github.com>
2025-10-17 13:04:10 +0300

    add navbar ui

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router";
+import Navbar from "@/components/partials/Navbar";
 
 const MainLayout = () => {
   return (
     <>
       <div className="font-inter">
+        <Navbar />
         <Outlet />
       </div>
     </>
