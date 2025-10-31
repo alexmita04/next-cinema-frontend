af206995e96b78e9effd9c23408f2368353cb97a
alexmita04 <72689514+alexmita04@users.noreply.github.com>
2025-10-31 10:58:21 +0200

    style alert component

--- src/components/items/AlertEl.tsx
+++ src/components/items/AlertEl.tsx
@@ -16,31 +16,6 @@ const AlertEl = ({ variant = "default", title, message }: AlertElInterface) => {
           <AlertTitle>{title}</AlertTitle>
           <AlertDescription>{message}</AlertDescription>
         </Alert>
-        {/* <Alert>
-          <CheckCircle2Icon />
-          <AlertTitle>Success! Your changes have been saved</AlertTitle>
-          <AlertDescription>
-            This is an alert with icon, title and description.
-          </AlertDescription>
-        </Alert>
-        <Alert>
-          <PopcornIcon />
-          <AlertTitle>
-            This Alert has a title and an icon. No description.
-          </AlertTitle>
-        </Alert>
-        <Alert variant="destructive">
-          <AlertCircleIcon />
-          <AlertTitle>Unable to process your payment.</AlertTitle>
-          <AlertDescription>
-            <p>Please verify your billing information and try again.</p>
-            <ul className="list-inside list-disc text-sm">
-              <li>Check your card details</li>
-              <li>Ensure sufficient funds</li>
-              <li>Verify billing address</li>
-            </ul>
-          </AlertDescription>
-        </Alert> */}
       </div>
     </>
   );
